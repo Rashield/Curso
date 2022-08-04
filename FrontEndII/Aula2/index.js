a1dc35749c0b1b49792709225cc2ea10b819dc45
Darlei Pedro Feix <darleifeix97@gmail.com>
2022-08-03 22:14:24 -0300

    add Aula2 FEII

---
+++ FrontEndII/Aula2/index.js
@@ -0,0 +1,46 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+let winsUser=0, winsPC=0;
+
+while(winsPC<2 && winsUser<2){
+    let escolhaUser = prompt("Pedra,papel ou tesoura!");
+    let escolhaPC = getRandomInt(1,4);
+    
+    if(escolhaPC==1){
+        escolhaPC = "Pedra";
+    }else if(escolhaPC==2){
+        escolhaPC = "Papel";
+    }else if(escolhaPC==3){
+        escolhaPC = "Tesoura";
+    }
+    
+    if(escolhaUser == "Pedra" || escolhaUser == "pedra"){
+        if(escolhaPC=="Papel"){
+            winsPC++;
+        }else if(escolhaPC=="Tesoura"){
+            winsUser++;
+        }else{};
+    }else if(escolhaUser=="Papel" || escolhaUser=="papel"){
+        if(escolhaPC=="Tesoura"){   
+            winsPC++;
+        }else if(escolhaPC=="Pedra"){
+            winsUser++;
+        }else{};
+    }else{
+        if(escolhaPC=="Papel"){
+            winsUser++;
+        }else if(escolhaPC=="Pedra"){
+            winsPC++;
+        }else {};
+    }
+    alert("Sua escolha - "+escolhaUser+"        "+winsUser+"  :  "+winsPC+"         "+escolhaPC+" - Escolha da máquina");
+}
+
+if(winsPC==2){
+    alert("Você perdeu!!!");
+}else{
+    alert("Você ganhou!!!");
+};
